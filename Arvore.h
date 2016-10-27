a8780ccb3e6eeed40cfebbfbc83c833b582d2447
cmcarlosmoura <cm_carlosmoura@hotmail.com>
2016-10-27 13:25:20 -0300

    Update Arvore.h

--- Arvore.h
+++ Arvore.h
@@ -1,29 +1,28 @@
 
 
-class No
-{
-	public:
-		No();
-        int valor;
-        No *esq;
-		No *dir;
-	
-};
-
 class Arvore
 {
+    struct n{
+        int valor;
+        struct n *esq;
+		struct n *dir;
+    };
+	
+	typedef struct n No;
 
     public:
         Arvore();
         void imprime(void);
         void insere(int);
+        
         void busca(int);
-        No *raiz;
+        
+        No* raiz;
     
 	private:
-		void imprime(No*);
-    	void insere(No*,int);
-    	void busca(No*,int);
+		void imprime(No**);
+    	void insere1(No **,int);
+    	void busca(No**,int);
         int cont;
         
 
