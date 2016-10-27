a3102cb9d6e71e1e8946374cffa0bf60ff57ebfc
cmcarlosmoura <cm_carlosmoura@hotmail.com>
2016-10-27 13:04:43 -0300

    Update Arvore.h

--- Arvore.h
+++ Arvore.h
@@ -1,1 +1,31 @@
 
+
+class No
+{
+	public:
+		No();
+        int valor;
+        No *esq;
+		No *dir;
+	
+};
+
+class Arvore
+{
+
+    public:
+        Arvore();
+        void imprime(void);
+        void insere(int);
+        void busca(int);
+        No *raiz;
+    
+	private:
+		void imprime(No*);
+    	void insere(No*,int);
+    	void busca(No*,int);
+        int cont;
+        
+
+};
+
